d13aa263e4513d6513d62b41fca3a86c2b3df064
NFicano <nick@keep.com>
2013-11-12 02:33:14 -0500

    flushed out most of the core functionality in half the code

--- pytube/youtube.py
+++ pytube/youtube.py
@@ -2,7 +2,7 @@
 
 from urllib import urlencode
 from urllib2 import urlopen
-from urlparse import urlparse, parse_qs, unquote, parse_qsl
+from urlparse import urlparse, parse_qs
 from os.path import join
 
 BASE_URL = 'http://www.youtube.com/get_video_info'
@@ -34,37 +34,25 @@ ITAGS = {
     102: ("webm", "720p", "VP8", "3D", 192)
 }
 
+
 class YouTube(object):
-    metadata = {}
-    video_id = None
 
     def __init__(self, url):
-        ok, video_id = self.extract_id_from_url(url)
-        self.video_id = video_id
+        self.url = url
+        self.video_id = self.get_id_by_url(url)
 
-    def extract_id_from_url(self, url):
+    def get_id_by_url(self, url):
         parts = urlparse(url)
         if hasattr(parts, 'query'):
             query_str = parse_qs(parts.query)
             if 'v' in query_str and len(query_str['v']) is 1:
-                return (True, query_str['v'][0])
-        return (False, None)
+                return query_str['v'][0]
 
-    def get_title(self):
-        metadata = self.get_metadata(self.video_id)
-        return metadata['title']
+    def mget_videos_by_id(self, video_id):
+        raise NotImplemented
 
-    def get_videos(self):
-        videos = []
-        metadata = self.get_metadata(self.video_id)
-        fsm = metadata['fmt_stream_map']
-        itag = [ITAGS.get(int(i), int(i)) for i in fsm['itag']]
-        urls = fsm['url']
-        return [Video(url, meta) for url, meta in zip(urls, itag)]
-
-
-    def get_metadata(self, video_id):
-        return self.metadata.get(video_id, self._extract_video_data(video_id))
+    def get_metadata_by_id(self, video_id):
+        return self._request(video_id)
 
     def _urlize(self, url, *paths, **query):
         paths = (join(*paths) if paths else '')
@@ -73,44 +61,46 @@ class YouTube(object):
             return ''.join([url, paths, '?', urlencode(query)])
         return ''.join([url, paths])
 
-    def _parse_qs(self, qs):
+    def _parse_query_string(self, qs):
+        """Parses query string to a coerced datatype.
+
+        :param qs: A uri query string
+        :returns: a flattened dict representation of a query sting
+        """
+
         d = {}
         for key, val in parse_qs(qs).iteritems():
             d[key] = (val if len(val) > 1 else val[0])
         return d
 
-    def _extract_video_data(self, video_id):
+    def _request(self, video_id):
+        """Executes request to YouTube v3 service detailpage and handles the
+        response, parsing out the meta data and decodes the format stream
+        map.
+
+        :param video_id: the YouTube video Id
+        :returns: a dict representation of the metadata
+        """
+
         url = self._urlize(BASE_URL, asv=3, el='detailpage', hl='en_US',
                            video_id=video_id)
 
         response = urlopen(url)
         if response.getcode() != 200:
-            return None
+            raise
 
-        body = self._parse_qs(response.read())
-        fsm = self._parse_qs(body['url_encoded_fmt_stream_map'])
+        metadata = self._parse_query_string(response.read())
 
-        body['fmt_stream_map'] = fsm
+        stream_map = [self._parse_query_string(fsm) for fsm in
+                      metadata['url_encoded_fmt_stream_map'].split(',')]
+        for fsm in stream_map:
+            fsm.update({'url': '{url}&signature={sig}'.format(**fsm)})
 
-        self.metadata[video_id] = body
-        return body
-
-class Video(object):
-    def __init__(self, url, itag):
-        self.url = url
-        self._parse_itag(*itag)
-
-    def _parse_itag(self, ext, res, v_codec, profile, a_bitrate):
-        self.ext = ext
-        self.res = res
-        self.video_codec = v_codec
-        self.profile = profile
-        self.audio_bitrate = a_bitrate
-
-    def __repr__(self):
-        return "<Video: {0} (.{1}) - {2}>".format(
-            self.video_codec, self.extension, self.resolution)
+        metadata['fmt_stream_map'] = stream_map
+        return metadata
 
 if __name__ == '__main__':
+    from pprint import pprint
     yt = YouTube("http://www.youtube.com/watch?v=Ik-RsDGPI5Y")
-    print yt.get_videos()
+    video_id = yt.video_id
+    pprint(yt.get_metadata_by_id(video_id))
