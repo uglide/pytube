27fab460d2ba426e0980f44089f9ad05a3dcc51b
NFicano <nick@keep.com>
2013-11-18 19:37:23 -0500

    docstrings now better formatted for Sphinx, added more comments, minor refactoring of utility methods, improving readablity at the expense of line numbers

--- pytube/youtube.py
+++ pytube/youtube.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
 
+"""
+.. module:: pytube.youtube
+   :platform: Unix
+   :synopsis: Library for downloading YouTube videos
+
+.. moduleauthor:: Nick Ficano <nficano@gmail.com>
+"""
+
 from urllib import urlencode
 from urllib2 import urlopen
 from urlparse import urlparse, parse_qs
@@ -13,88 +21,133 @@ class YouTube(object):
     """
 
     def __init__(self, url):
-        """Initialize class instance.
+        """
+        Initialize class instance.
 
         :param url: web address to YouTube video
+        :type url: str
         """
         self.url = url
         self.video_id = self.get_id_by_url(url)
 
     def get_id_by_url(self, url):
-        """Extracts the video Id from a YouTube url.
+        """
+        Extracts the video Id from a YouTube url.
 
         :param video_id: YouTube url
-        :returns: string video Id
+        :type video_id: str
+        :returns: YouTube video Id
+        :rtype: str
         """
 
+        #TODO: raise exception for malformed url, no '?v=xxx' found in query
+        #string. Also test on url-shortened youtu.be
+
+        #Split URI into 'parts', check if query string contains node 'v' and
+        #return accordingly.
         parts = urlparse(url)
         if hasattr(parts, 'query'):
             qs = self._parse_qs(parts.query)
             return qs.get('v')
 
     def mget_videos_by_id(self, video_id):
-        """multi-get (Redis-nomenclature) returns a list of object
-        representation of YouTube videos for a given video Id.
+        """
+        multi-get returns a list of object representation of YouTube videos for
+        a given video Id.
 
         :param video_id: YouTube video Id
-        :returns: list of Video objects
+        :type video_id: str
+        :returns: Video objects
+        :rtype: list
         """
 
         raise NotImplemented
 
     def get_metadata_by_id(self, video_id):
-        """Returns the YouTube meta data for a given Id.
+        """
+        Returns the YouTube meta data for a given Id.
 
         :param video_id: YouTube video Id
-        :returns: dict representation of the meta data
+        :type video_id: str
+        :returns: video meta data
+        :rtype: dict
         """
 
         return self._request(video_id)
 
     def _url(self, url, *paths, **qs):
-        """Lazily constructs a url given optional paths and query string as
+        """
+        Lazily constructs a url given optional paths and query string as
         keyword args.
 
-        :param url: base url (e.g.: http://example.com)
-        :param paths: optional path(s) to append to the url
-        :param qs: optional query string as keyword args
-        :returns: a properly formed url
+        :param url: The base URL (e.g.: http://example.com)
+        :type url: str
+        :param paths: (optional) Path(s) to append to url (e.g.: /path/to/file)
+        :type paths: list
+        :param qs: (optional) build query from keyword args (e.g.: ?foo=bar)
+        :type qs: dict
+        :returns: a formed url
+        :rtype: str
         """
 
-        paths = (join(*paths) if paths else '')
+        #Store url as list for easier assembly.
+        url_parts = [url]
 
-        if qs:
-            return ''.join([url, paths, '?', urlencode(qs)])
-        return ''.join([url, paths])
+        #TODO: Sanitize paths ensuring slashes are handled properly.
+        if isinstance(paths, list):
+            paths = join(*paths)
+            url_parts.append(paths)
+
+        #TODO: Sanitize query string ensuring no leading '?' or training '&'
+        if isinstance(qs, dict):
+            qs = '?' + urlencode(qs)
+            url_parts.append(qs)
+
+        return ''.join(url_parts)
 
     def _parse_qs(self, qs):
-        """Parse a query given as a string argument.
+        """
+        Parse a query given as a string argument. (Similar to parse_qs method
+        found in urlparse module except this handles the flattening of
+        unnecessary tuples.
 
         :param qs: uri query string
+        :type qs: str
         :returns: a flattened dict representation of a query string
+        :rtype: dict
         """
 
-        d = {}
+        d = dict()
+        #loop through object key/value yielded from parse_qs iteritems, check
+        #if the value contains one item (no child nodes), replace tuple with
+        #strings accordingly.
         for key, val in parse_qs(qs).iteritems():
             d[key] = (val if len(val) > 1 else val[0])
         return d
 
     def _request(self, video_id):
-        """Executes request to YouTube v3 service detailpage and handles the
+        """
+        Executes request to YouTube v3 service detailpage and handles the
         response, parsing out the meta data and decodes the format stream
         map.
 
         :param video_id: the YouTube video Id
+        :type video_id: str
         :returns: a dict representation of the metadata
+        :rtype: dict
         """
 
+        #Assemble URL and execute http request.
         url = self._url(BASE_URL, asv=3, el='detailpage', hl='en_US',
                         video_id=video_id)
-
         response = urlopen(url)
+
         if response.getcode() != 200:
+            #TODO: raise proper exception if status code is not 200.
             raise
 
+        #Parse the meta data found in the response body using modified version
+        #of parse_qs.
         metadata = self._parse_qs(response.read())
 
         stream_map = [self._parse_qs(fsm) for fsm in
